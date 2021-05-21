5f1a19414499a948c749fbec765fa47014943a10
Sergio Rios <sergio.rios@payco.co>
2021-05-21 13:05:19 -0500

    feat(apify service): add apify as service

---
+++ src/Service/Apify.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Service;
+
+use Exception;
+use Requests;
+use Requests_Auth_Basic;
+use Requests_Exception;
+use Requests_Session;
+
+class Apify
+{
+    /**
+     * @var Requests_Session
+     */
+    private $session;
+
+    private $url;
+
+    public function __construct()
+    {
+        $this->url = 'https://apify.epayco.xyz/';
+        $this->session = new Requests_Session($this->url, [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            'User-Agent' => 'newDash',
+        ], [], ['timeout' => 60]);
+    }
+
+    public function login(string $user, string $password)
+    {
+        try {
+            $auth = new Requests_Auth_Basic([
+                $user,
+                $password,
+            ]);
+        } catch (Requests_Exception $exception) {
+        }
+
+        $response = Requests::post($this->url . 'login/mail', [], [], [
+            'auth' => $auth,
+        ]);
+
+        if (!$response->success || 200 !== $response->status_code) {
+            throw new Exception('Apify login error');
+        }
+
+        $body = json_decode($response->body, true);
+
+        return $body;
+    }
+}
