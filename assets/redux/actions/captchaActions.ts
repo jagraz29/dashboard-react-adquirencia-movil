841a8c5a1b342119c67ee9684b2f7a4a9ec5cf58
Sergio Rios <sergio.rios@payco.co>
2021-05-25 15:51:53 -0500

    refactor: eliminacion de imports innecesarios

--- assets/redux/actions/captchaActions.ts
+++ assets/redux/actions/captchaActions.ts
@@ -4,7 +4,6 @@ export const SET_STATE_GLOBAL = 'SET_STATE_GLOBAL'
 const dataService = new DataService()
 
 export const setToken = (token: any) => (dispatch: any) => {
-  const valid = dataService.validateRecaptcha(token)
   dispatch({
     type: SET_STATE_GLOBAL,
     payload: token,
