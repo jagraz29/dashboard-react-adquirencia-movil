d9eafff10f814a342f05c29713cecd2905986935
Angelo Rojas <angelog211198@gmail.com>
2021-06-16 19:25:23 -0400

    feat: mas constantes

--- src/Common/TextResponsesCommon.php
+++ src/Common/TextResponsesCommon.php
@@ -5,13 +5,24 @@ namespace App\Common;
 final class TextResponsesCommon
 {
   const AUTHORIZATION = 'Authorization';
+  const ASUNTO = 'asunto';
   const BEARER = 'Bearer';
   const DATA = 'data';
   const EMAIL = 'email';
+  const CELULAR = 'celular';
+  const INDICATIVO_PAIS = 'indicativoPais';
+  const INDICATIVO_CIUDAD = 'indicativoCiudad';
   const FILTER = 'filter';
+  const FILES = 'files';
   const LIMIT = 'limit';
+  const NOMBRE_EMPRESA = 'nombreEmpresa';
   const PAGINATION = 'pagination';
+  const PASSWORD = 'password';
+  const PREGUNTA = 'pregunta';
+  const RAZON_SOCIAL = 'razonSocial';
+  const RESPUESTA = 'respuesta';
   const SUCCESS = 'success';
+  const TELEFONO = 'telefono';
   const TEXT_RESPONSE = 'textResponse';
   const TOKEN = 'token';
 }
