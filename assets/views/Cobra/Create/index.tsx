e53369d4aa3c87c7deb9d603fb34cbab156e831f
Angelog21 <angelog211198@gmail.com>
2021-06-16 00:15:04 -0400

    feat: cobra maquetaciòn lista y carga de archivos

--- assets/views/Cobra/Create/index.tsx
+++ assets/views/Cobra/Create/index.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useState, useRef, useCallback } from 'react'
 import * as BsIcons from 'react-icons/bs'
 
 import Title from '../../../components/Title'
@@ -9,28 +8,34 @@ import TextareaCustomer from '../../../components/TextareaCustomer'
 import InputLabel from '../../../components/InputLabel'
 import CustomSwitch from '../../../components/Switch'
 import InputSelect from '../../../components/InputSelect'
-import FileUpload from '../../../components/FileUpload'
-import { RootState } from '../../../redux/reducers/index'
-import { getPropertySite } from '../../../redux/actions/'
+import Dropzone from 'react-dropzone'
 
 import {
+  Input,
   Content,
   ContentCard,
   Card,
   CardHeader,
+  LoadImage,
+  ClosePhoto,
   CardContent1,
   CardContent2,
   CardContent3,
   CardTitle,
   CardSubTitle,
   CardIcon,
+  ImageShow,
   ContentInput,
   CardContentButton,
-  CardButton,
+  ButtonImageLoad,
   ButtonOk,
   ButtonCancel,
   ContentInputCard,
+  ContentInputImageCard,
   InputGroup,
+  PhotoDropLoaded,
+  DropLoaded,
+  DropDocArea,
 } from './styles'
 
 const breadcrumb = [
@@ -46,8 +51,40 @@ const breadcrumb = [
   },
 ]
 
-const CobraCreate = () => {
+interface arrayFileInterface {
+  lastModified: number
+  lastModifiedDate: any
+  name: string
+  path: string
+  size: number
+  type: string
+  webkitRelativePath: any
+}
+
+interface cobroInterface {
+  [key: string]: any
+}
+
+const CobraCreate = (props: any) => {
   const iconStyles = { color: '#d3d3d3' }
+
+  const [cobro, setCobro] = useState<cobroInterface>({
+    nombre: '',
+    descripcion: '',
+    factura: '',
+    tipoMoneda: 'COP',
+    valor: 0,
+    impuestos: 0,
+    imagenes: [],
+    archivo: null,
+    cantidad: 0,
+    fechaVencimiento: new Date(),
+    urlConfirmacion: '',
+    urlRespuesta: '',
+  })
+  const [loadImages, setLoadImages] = useState<arrayFileInterface[]>([])
+  const [loadFiles, setLoadFiles] = useState<arrayFileInterface[]>([])
+
   const [openCard, setOpenCard] = useState(true)
   const [openCard2, setOpenCard2] = useState(false)
   const [openCard3, setOpenCard3] = useState(false)
@@ -55,6 +92,44 @@ const CobraCreate = () => {
   const [openCardContent, setOpenCardContent] = useState({ display: 'block' })
   const [openCardContent2, setOpenCardContent2] = useState({ display: 'none' })
   const [openCardContent3, setOpenCardContent3] = useState({ display: 'none' })
+
+  const onDropImg = (accepted: any, rejected: any) => {
+    if (rejected.length > 0) {
+      console.log('Solo puede subir archivos con extención .jpg, .jpeg, .png, .gif')
+    } else {
+      if (accepted.length > 3 - loadImages.length) {
+        console.log('solo puede cargar hasta 3 imágenes.')
+      } else {
+        setLoadImages((prev) => prev.concat(accepted))
+      }
+    }
+  }
+
+  const onDropDoc = (accepted: any, rejected: any) => {
+    if (rejected.length > 0) {
+      console.log('error al subir el archivo pdf.')
+    } else {
+      setLoadFiles((prev) => prev.concat(accepted))
+    }
+  }
+
+  const deletedoc = () => {
+    setLoadFiles([])
+  }
+
+  const deleteFile = (numero: number) => {
+    const currentfiles = [...loadImages]
+    currentfiles.splice(numero, 1)
+    setLoadImages([...currentfiles])
+  }
+
+  const handleChangeInput = useCallback((e: any) => {
+    const target = e.target
+    const name = target.name
+    const value = target.value
+
+    setCobro({ ...cobro, [name]: value })
+  }, [])
 
   const openClose = async (number: number) => {
     switch (number) {
@@ -109,21 +184,20 @@ const CobraCreate = () => {
           <Card>
             <CardHeader>
               <CardTitle>Nuevo Link de Cobro</CardTitle>
+              <CardSubTitle>Datos principales del link de cobro.</CardSubTitle>
             </CardHeader>
 
             <CardContent1 theme={openCardContent}>
               <ContentInput>
                 <ContentInputCard>
                   <InputLabel label={'¿Que cobra?'} />
-                  <InputCustumer
-                    name={'razon_social'}
+                  <Input
+                    name={'nombre'}
                     type={'text'}
-                    placeholder={'Razon social'}
+                    placeholder={'Titulo'}
                     width={'40vw'}
-                    value={'hola2'}
-                    onChange={(e: any) => {
-                      console.log(e)
-                    }}
+                    value={cobro.nombre}
+                    onChange={handleChangeInput}
                   />
                 </ContentInputCard>
               </ContentInput>
@@ -132,16 +206,14 @@ const CobraCreate = () => {
                 <ContentInputCard>
                   <InputLabel label={'Descripción'} />
                   <TextareaCustomer
-                    name={'nombre_mostrar'}
+                    name={'descripcion'}
                     type={'text'}
                     placeholder={
                       'Describe el producto o servicio, sus características y disponibilidad'
                     }
                     width={'22.3vw'}
-                    value={'hola3'}
-                    onChange={(e: any) => {
-                      console.log(e)
-                    }}
+                    value={cobro.descripcion}
+                    onChange={handleChangeInput}
                   />
                 </ContentInputCard>
               </ContentInput>
@@ -152,14 +224,12 @@ const CobraCreate = () => {
                 <ContentInputCard>
                   <InputLabel label={'# Factura'} />
                   <InputCustumer
-                    name={'razon_social'}
+                    name={'factura'}
                     type={'text'}
                     placeholder={'Ej: P0001'}
                     width={'40vw'}
-                    value={'hola2'}
-                    onChange={(e: any) => {
-                      console.log(e)
-                    }}
+                    value={cobro.factura}
+                    onChange={handleChangeInput}
                   />
                 </ContentInputCard>
               </ContentInput>
@@ -171,27 +241,24 @@ const CobraCreate = () => {
                   <InputLabel label={'¿Cuanto Vale?'} />
                   <InputGroup>
                     <InputSelect
-                      name={'type_telefono'}
+                      name={'tipoMoneda'}
                       placeholder={'Seleccione Moneda'}
                       width={'12vw'}
-                      dataSelect={[]}
-                      onClick={() => {
-                        console.log('hola')
-                      }}
-                      onChange={(e: any) => {
-                        console.log(e)
-                      }}
+                      dataSelect={[
+                        { value: 'COP', label: 'COP' },
+                        { value: 'USD', label: 'USD' },
+                      ]}
+                      onChange={handleChangeInput}
+                      onClick={handleChangeInput}
                     />
 
                     <InputCustumer
-                      name={'telefono'}
+                      name={'valor'}
                       type={'number'}
-                      placeholder={'Telefono'}
+                      placeholder={'10'}
                       width={'12vw'}
-                      value={'hhhhh'}
-                      onChange={(e: any) => {
-                        console.log(e)
-                      }}
+                      value={cobro.valor}
+                      onChange={handleChangeInput}
                     />
                   </InputGroup>
                 </ContentInputCard>
@@ -215,13 +282,13 @@ const CobraCreate = () => {
           </Card>
 
           <Card>
-            <CardHeader>
+            <CardHeader onClick={() => openClose(2)}>
               <CardTitle>Personalizar Cobro</CardTitle>
               <CardSubTitle>
                 Suba imágenes, especificaciones de su producto y/o servicio de un archivo de
-                caraterísticas, inventario y fecha de vencimiento del link de cobro.
+                caraterísticas del link de cobro.
               </CardSubTitle>
-              <CardIcon onClick={() => openClose(2)}>
+              <CardIcon>
                 {openCard2 == true ? (
                   <BsIcons.BsFillCaretUpFill style={iconStyles} />
                 ) : (
@@ -231,32 +298,117 @@ const CobraCreate = () => {
             </CardHeader>
 
             <CardContent2 theme={openCardContent2}>
-              <ContentInput>
+              <ContentInput style={{ borderBottom: '0.1vw solid #d3d3d3' }}>
+                <ContentInputImageCard>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <InputLabel label={'Agregue imágenes del producto'} />
+                    <i style={{ fontSize: '0.7vw', marginLeft: '1vw', color: '#d3d3d3' }}>
+                      (Máximo 3 imágenes en formato .jpeg, .png, .jpg, .gif)
+                    </i>
+                  </div>
+                  <div style={{ display: 'flex' }}>
+                    <PhotoDropLoaded>
+                      {loadImages.map((image, index) => (
+                        <LoadImage key={`${image.name}-${index}`}>
+                          <ImageShow src={URL.createObjectURL(image)} alt="" />
+                          <ClosePhoto onClick={() => deleteFile(index)}>
+                            <small>Eliminar</small>
+                          </ClosePhoto>
+                        </LoadImage>
+                      ))}
+                    </PhotoDropLoaded>
+                    {loadImages.length < 3 && (
+                      <Dropzone
+                        accept="image/jpeg, image/png, image/jpg"
+                        onDrop={onDropImg}
+                        maxSize={5242880}
+                      >
+                        {({ getRootProps, getInputProps }) => (
+                          <ButtonImageLoad {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <img
+                              style={{ width: '2vw' }}
+                              src="https://multimedia.epayco.co/dashboard/image.png"
+                              alt=""
+                            />
+                          </ButtonImageLoad>
+                        )}
+                      </Dropzone>
+                    )}
+                  </div>
+                </ContentInputImageCard>
+              </ContentInput>
+
+              <ContentInput style={{ borderBottom: '0.1vw solid #d3d3d3' }}>
+                <ContentInputImageCard>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <InputLabel label={'Suba indicaciones o especificaciones del producto'} />
+                    <i style={{ fontSize: '0.7vw', marginLeft: '1vw', color: '#d3d3d3' }}>
+                      (Máximo 1 archivo en formato .pdf)
+                    </i>
+                  </div>
+                  <div style={{ display: 'flex', margin: '1vw 0' }}>
+                    {loadFiles.map((file) => (
+                      <DropLoaded key={file.name}>
+                        <span>
+                          <i className={'fa fa-file mr-2'}></i> {file.name}
+                        </span>
+                        <div className="close" onClick={deletedoc}>
+                          <BsIcons.BsFillTrashFill style={{ color: 'black' }} />
+                        </div>
+                      </DropLoaded>
+                    ))}
+                    {loadFiles.length < 1 && (
+                      <Dropzone
+                        multiple={false}
+                        accept="application/pdf"
+                        onDrop={onDropDoc}
+                        maxSize={5242880}
+                      >
+                        {({ getRootProps, getInputProps }) => (
+                          <DropDocArea {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <BsIcons.BsCloudUpload
+                              style={{ color: '#40A8E6', marginLeft: '1vw' }}
+                            />
+                            <p>Subir archivo</p>
+                          </DropDocArea>
+                        )}
+                      </Dropzone>
+                    )}
+                  </div>
+                </ContentInputImageCard>
+              </ContentInput>
+
+              <ContentInput style={{ borderBottom: '0.1vw solid #d3d3d3' }}>
                 <ContentInputCard>
-                  <InputLabel label={'Url de respuesta'} />
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <InputLabel label={'Cantidad'} />
+                    <i style={{ fontSize: '0.7vw', marginLeft: '1vw', color: '#d3d3d3' }}>
+                      (Las veces que se hará el cobro)
+                    </i>
+                  </div>
                   <InputCustumer
-                    name={'url_respuesta'}
-                    type={'text'}
-                    placeholder={'Url donde el cliente es redireccionado al finalizar'}
-                    width={'22.3vw'}
-                    value={''}
-                    onChange={() => {
-                      console.log('holaa')
-                    }}
+                    name={'cantidad'}
+                    type={'number'}
+                    placeholder={'0'}
+                    width={'4vw'}
+                    value={cobro.cantidad}
+                    onChange={handleChangeInput}
                   />
                 </ContentInputCard>
+              </ContentInput>
 
+              <ContentInput>
                 <ContentInputCard>
-                  <InputLabel label={'Url de confirmación'} />
+                  <InputLabel label={'Fecha de vencimiento'} />
                   <InputCustumer
-                    name={'url_confirmacion'}
-                    type={'text'}
-                    placeholder={'Url donde se envia la confirmación de la transacción'}
-                    width={'22.3vw'}
-                    value={''}
-                    onChange={() => {
-                      console.log('Conf')
-                    }}
+                    name={'fechaVencimiento'}
+                    type={'date'}
+                    placeholder={'0'}
+                    width={'40vw'}
+                    value={cobro.fechaVencimiento}
+                    onChange={handleChangeInput}
                   />
                 </ContentInputCard>
               </ContentInput>
@@ -264,12 +416,12 @@ const CobraCreate = () => {
           </Card>
 
           <Card>
-            <CardHeader>
+            <CardHeader onClick={() => openClose(3)}>
               <CardTitle>URL de Confirmación</CardTitle>
               <CardSubTitle>
                 Ingrese una página de confirmación y/o agradecimiento por la compra.
               </CardSubTitle>
-              <CardIcon onClick={() => openClose(3)}>
+              <CardIcon>
                 {openCard3 == true ? (
                   <BsIcons.BsFillCaretUpFill style={iconStyles} />
                 ) : (
@@ -285,13 +437,13 @@ const CobraCreate = () => {
                     name={'url_confirmacion'}
                     type={'text'}
                     placeholder={'https://mi-tienda.com/page-response'}
-                    width={'22.3vw'}
+                    width={'40vw'}
                     value={''}
                     onChange={() => {
                       console.log('Conf')
                     }}
                   />
-                  <i style={{ fontSize: '0.5vw' }}>
+                  <i style={{ fontSize: '0.7vw' }}>
                     (Url donde se envia la confirmación de la transacción)
                   </i>
                 </ContentInputCard>
@@ -303,13 +455,13 @@ const CobraCreate = () => {
                     name={'url_respuesta'}
                     type={'text'}
                     placeholder={'https://mi-tienda.com/page-confirmation'}
-                    width={'22.3vw'}
+                    width={'40vw'}
                     value={''}
                     onChange={() => {
                       console.log('holaa')
                     }}
                   />
-                  <i style={{ fontSize: '0.5vw' }}>
+                  <i style={{ fontSize: '0.7vw' }}>
                     (Url donde el cliente es redireccionado al finalizar la transacción)
                   </i>
                 </ContentInputCard>
