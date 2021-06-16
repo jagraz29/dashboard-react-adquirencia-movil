1b925faa94c4c18c2e656099d4fc810985900576
Sergio Rios <sergio.rios@payco.co>
2021-06-16 16:24:59 -0500

    feat: se crea common para textos comunes para validaciones de sonar

---
+++ src/Common/TextResponsesCommon.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Common;
+
+final class TextResponsesCommon
+{
+  const DATA = 'data';
+  const FILTER = 'filter';
+  const LIMIT = 'limit';
+  const PAGINATION = 'pagination';
+  const SUCCESS = 'success';
+  const TEXT_RESPONSE = 'textResponse';
+}
