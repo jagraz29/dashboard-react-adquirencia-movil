651908fce07ee1194c633c2be470583beb1f5124
Juan Felipe Mesa Ocampo <felipemesa14@gmail.com>
2021-07-26 17:56:19 -0500

    fix: variables de entorno

--- assets/config/configImages.ts
+++ assets/config/configImages.ts
@@ -1,67 +1,69 @@
 export const LogoDavivienda = {
   name: 'LogoDavivienda',
-  url: process.env.URL_S3_IMAGES + '/adquirencia-movil/Logos/davivienda+-+white.png',
+  url: process.env.REACT_APP_AMAZON_URL + '/adquirencia-movil/Logos/davivienda+-+white.png',
 }
 
 export const LogoEpayCo = {
   name: 'LogoEpayCo',
-  url: process.env.URL_S3_IMAGES + '/adquirencia-movil/Logos/epayco+-+white.png',
+  url: process.env.REACT_APP_AMAZON_URL + '/adquirencia-movil/Logos/epayco+-+white.png',
 }
 
 export const IconHome = {
   name: 'IconHome',
-  url: process.env.URL_S3_IMAGES + '/adquirencia-movil/Logo+sidebar/house-outline.png',
+  url: process.env.REACT_APP_AMAZON_URL + '/adquirencia-movil/Logo+sidebar/house-outline.png',
 }
 
 export const IconCobra = {
   name: 'IconCobra',
-  url: process.env.URL_S3_IMAGES + '/adquirencia-movil/Logo+sidebar/hand.png',
+  url: process.env.REACT_APP_AMAZON_URL + '/adquirencia-movil/Logo+sidebar/hand.png',
 }
 
 export const IconTransaction = {
   name: 'IconTransaction',
-  url: process.env.URL_S3_IMAGES + '/adquirencia-movil/Logo+sidebar/bar-chart.png',
+  url: process.env.REACT_APP_AMAZON_URL + '/adquirencia-movil/Logo+sidebar/bar-chart.png',
 }
 
 export const IconIntegration = {
   name: 'IconIntegration',
-  url: process.env.URL_S3_IMAGES + '/adquirencia-movil/Logo+sidebar/Group+491.png',
+  url: process.env.REACT_APP_AMAZON_URL + '/adquirencia-movil/Logo+sidebar/Group+491.png',
 }
 
 export const IconSequrity = {
   name: 'IconSequrity',
-  url: process.env.URL_S3_IMAGES + '/adquirencia-movil/Logo+sidebar/escudo-seguro+1.png',
+  url: process.env.REACT_APP_AMAZON_URL + '/adquirencia-movil/Logo+sidebar/escudo-seguro+1.png',
 }
 
 export const IconLink = {
   name: 'IconLink',
-  url: process.env.URL_S3_IMAGES + '/adquirencia-movil/link+1.png',
+  url: process.env.REACT_APP_AMAZON_URL + '/adquirencia-movil/link+1.png',
 }
 
 export const IconVigilado = {
   name: 'IconVigilado',
-  url: process.env.URL_S3_IMAGES + '/adquirencia-movil/Logos/superintendencia.png',
+  url: process.env.REACT_APP_AMAZON_URL + '/adquirencia-movil/Logos/superintendencia.png',
 }
 
 export const IconEpeycoFooter = {
   name: 'IconEpeycoFooter',
-  url: process.env.URL_S3_IMAGES + '/adquirencia-movil/Logos/logoepayco.png',
+  url: process.env.REACT_APP_AMAZON_URL + '/adquirencia-movil/Logos/logoepayco.png',
 }
 
 export const IconService = {
   name: 'IconService',
-  url: process.env.URL_S3_IMAGES + '/adquirencia-movil/Logo+sidebar/servicio.png',
+  url: process.env.REACT_APP_AMAZON_URL + '/adquirencia-movil/Logo+sidebar/servicio.png',
 }
 export const IconSoporte = {
   name: 'IconSoporte',
-  url: process.env.URL_S3_IMAGES + '/dashboard/support.png',
+  url: process.env.REACT_APP_AMAZON_URL + '/dashboard/support.png',
 }
 
 export const CardsIcons: any = {
-  VS: process.env.URL_S3_IMAGES + '/adquirencia-movil/Medios+de+pago/VS+Cr%E2%80%9Adito+Visa.png',
+  VS:
+    process.env.REACT_APP_AMAZON_URL +
+    '/adquirencia-movil/Medios+de+pago/VS+Cr%E2%80%9Adito+Visa.png',
   MS:
-    process.env.URL_S3_IMAGES +
+    process.env.REACT_APP_AMAZON_URL +
     '/adquirencia-movil/Medios+de+pago/MS+Cr%E2%80%9Adito+Mastercard.png',
-  PSE: process.env.URL_S3_IMAGES + '/adquirencia-movil/Medios+de+pago/PSE+PSE.png',
-  DEFAULT: process.env.URL_S3_IMAGES + '/dashboard/cards/N/A.png',
+  PSE: process.env.REACT_APP_AMAZON_URL + '/adquirencia-movil/Medios+de+pago/PSE+PSE.png',
+  DEFAULT: process.env.REACT_APP_AMAZON_URL + '/dashboard/cards/N/A.png',
 }
